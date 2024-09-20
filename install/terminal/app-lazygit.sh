95ac1761a963a98191c8698241b72ad711e31718
Ian <ian.poxon@protonmail.com>
2024-09-20 21:43:31 +0100

    install git and git-lfs before lazygit

--- install/terminal/app-lazygit.sh
+++ install/terminal/app-lazygit.sh
@@ -1,3 +1,10 @@
+#!/usr/bin/env bash
+
+sudo apt install -y git
+
+curl -s https://packagecloud.io/install/repositories/github/git-lfs/script.deb.sh | sudo bash
+sudo apt install git-lfs
+
 cd /tmp
 LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | grep -Po '"tag_name": "v\K[^"]*')
 curl -sLo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/latest/download/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"

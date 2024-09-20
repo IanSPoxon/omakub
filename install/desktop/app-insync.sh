9f0c56d1cf945e798e280875a5fd2eadb932b850
Ian <ian.poxon@protonmail.com>
2024-09-20 21:43:04 +0100

    add insync installer

---
+++ install/desktop/app-insync.sh
@@ -0,0 +1,5 @@
+cd /tmp
+wget https://cdn.insynchq.com/builds/linux/3.9.4.60020/insync_3.9.4.60020-jammy_amd64.deb
+sudo apt install -y ./insync_3.9.4.60020-jammy_amd64.deb
+rm insync_3.9.4.60020-jammy_amd64.deb
+cd -
